6eec1b6dd643ab4fe5e155273c0cd356bf5e2fbe
jpegie <polygalov.vladimir@gmail.com>
2022-01-29 23:47:58 +0500

    Изменяю название семафора
    requestLimiter -> freeStreamsHandlingRequests

--- ServerConsoleApp/Server.cs
+++ ServerConsoleApp/Server.cs
@@ -10,16 +10,16 @@ namespace ServerConsoleApp
         const string ip = "127.0.0.1";
         const int port = 8080;
 
-        static int maxReqAmnt = 0;
+        static int maxRequestsAmnt = 0;
         static int clientsAmnt = 16;
 
         static void Main(string[] args)
         {
             #region SERVER_CONFIGURATION
             
-            Console.Write("[server] макс. кол-во запросов серверу : "); maxReqAmnt = int.Parse(Console.ReadLine());
+            Console.Write("[server] макс. кол-во запросов серверу : "); maxRequestsAmnt = int.Parse(Console.ReadLine());
 
-            SemaphoreSlim requestLimiter = new SemaphoreSlim(maxReqAmnt, maxReqAmnt);
+            SemaphoreSlim freeStreamsHandlingRequests = new SemaphoreSlim(maxRequestsAmnt, maxRequestsAmnt);
             IPEndPoint tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
             Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             
@@ -30,33 +30,33 @@ namespace ServerConsoleApp
             
             #endregion
 
-            for (int i = 0; i < maxReqAmnt + 1; ++i)
+            for (int i = 0; i < maxRequestsAmnt + 1; ++i)
             {
                 new Thread(() =>
                 {
                     while (true)
                     {
                         //изначально запустим на 1 поток больше, чтобы он мониторил запросы, которые способствуют перегрузке сервера
-                        Socket socketRequestListener = tcpSocket.Accept();
-                        RequestGetter requestGetter = new RequestGetter(socketRequestListener);
+                        Socket clientSocket = tcpSocket.Accept();
+                        RequestGetter requestGetter = new RequestGetter(clientSocket);
 
                         IRequest request = new Request();
-                        IResponseSender responseSender = new ResponseSender(socketRequestListener);
+                        IResponseSender responseSender = new ResponseSender(clientSocket);
                         
-                        if (requestLimiter.CurrentCount == 0)
+                        if (freeStreamsHandlingRequests.CurrentCount == 0)
                         {   
                             responseSender.SendStateAsResponse(States.ServerOverloaded); //если "слоты для обработку" закончились, то скажем об этом клиенту, отправив обратно ServerOverloaded статус
                         }
                         else
                         {
-                            requestLimiter.Wait();
+                            freeStreamsHandlingRequests.Wait();
                             request = requestGetter.GetRequest();
                             if (request.State != States.TryAgain && request.State != States.ServerOverloaded) //если не было ошибок, то определим полиндромность строки запроса
                             {
                                 request.State = new PalindromeChecker(request).GetPalindromeState();
                             }
                             responseSender.SendStateAsResponse(request.State); 
-                            requestLimiter.Release();
+                            freeStreamsHandlingRequests.Release();
                         }
                     }
                 }).Start();
